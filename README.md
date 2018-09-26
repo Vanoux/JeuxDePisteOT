cea03ffc70022eace5be007989291c4e773b955a
Groutch <hurizen1@hotmail.com>
2018-09-26 15:29:39 +0200

    rework with node/express/ejs

--- README.md
+++ README.md
@@ -1,31 +1,4 @@
 
-# JeuxDePisteOT
-
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
-
-# run unit tests
-npm run unit
-
-# run e2e tests
-npm run e2e
-
-# run all tests
-npm test
-```
-
-##Projet Pédago Jeux de Piste pour l'Office de Tourisme de Saint Gaudens
-
 ###Equipe Valentin, Clément, Vanessa
 
 Kanban : https://trello.com/b/tdAMBcN0/office-du-tourisme
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const bodyparser = require('body-parser');
+app.use(express.static('public'));
+// Création de la connexion de mysql avec le site
+let connection = mysql.createConnection({
+    host: 'den1.mysql1.gear.host ',
+    user: 'stgo',
+    password: 'Ek7eg2-7?6Vc',
+    database: 'stgo'
+});
+//Utilisation de body-parser par le serveur
+app.use(bodyparser.urlencoded({extended: false}));
+
+// Définition du moteur de template
+app.set('view engine', 'ejs');
+
+// Définition de la route racine
+app.get("/", function (req, res) {
+
+});
+
+// Lancement du serveur
+const server = app.listen(process.env.PORT || 8080, (req, res) =>
+    console.log('Server Ready')
+);
