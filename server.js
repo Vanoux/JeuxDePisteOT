48bfefd0b5634a78a3c1719cd37f490e701060db
Alencruser <alencruserxx@hotmail.fr>
2018-09-27 11:46:10 +0200

    fonction securisation input et gestion erreurs de celles-ci

--- server.js
+++ server.js
@@ -23,7 +23,17 @@ app.use(bodyparser.urlencoded({extended: false}));
 
 // Définition du moteur de template
 app.set('view engine', 'ejs');
+//Fonction sécurité sur les inputs
+function blbl(str) {
+	if (str == null) return '';
 
+	return String(str).
+	replace(/&/g, '&amp;').
+	replace(/</g, '&lt;').
+	replace(/>/g, '&gt;').
+	replace(/"/g, '&quot;').
+	replace(/'/g, '&#039;');
+};
 // Définition de la route racine
 app.get("/", function (req, res) {
 	sess=req.session;
@@ -44,26 +54,30 @@ app.get('/register',function(req,res){
 //Lors d'une tentative de connexion
 app.post('/login',function(req,res){
 	sess=req.session;
-	let username=req.body.username;
-	let password=req.body.password;
+	let username=blbl(req.body.username);
+	let password=blbl(req.body.password);
 	let connect=`SELECT pass FROM User WHERE username='${username}'`;
-	connection.query(connect,function(error,results,field){
-		if(error){
-			console.log(error);
-		}
-		else if(password==results[0].pass){
-			sess.username=username;
-			res.redirect('/');
-		}else {
-			res.redirect('/login')
-		}		
-	});
+	if(username.length>0 && password.length>0){
+		connection.query(connect,function(error,results,field){
+			if(error){
+				console.log(error);
+			}
+			else if(password==results[0].pass){
+				sess.username=username;
+				res.redirect('/');
+			}else {
+				res.redirect('/login')
+			}		
+		});
+	}else {
+		res.redirect('/login');
+	}
 });
 //Lors d'une inscription
 app.post('/register',function(req,res){
-	let username=req.body.username;
-	let pass=req.body.password;
-	let email=req.body.email
+	let username=blbl(req.body.username);
+	let pass=blbl(req.body.password);
+	let email=blbl(req.body.email);
 	let createAccount = `INSERT INTO User (username,pass,mail) VALUES ('${username}','${pass}','${email}')`;
 	connection.query(createAccount,function(error,results,field){
 		if(error){
