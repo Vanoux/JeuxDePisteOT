da72397fee79a74633c5179eb56cccdd716be0c3
Alencruser <alencruserxx@hotmail.fr>
2018-10-03 14:15:54 +0200

    ajout de plusieurs tests unitaire pour veiller au bon fonctionnement de deux fonctions essentielles à l'application

--- test/test.js
+++ test/test.js
@@ -1,16 +1,42 @@
 let chai = require('chai');
-let chaiHttp = require('chai-http');
+let expect = chai.expect;
 let should = chai.should();
 let server = require('../server.js');
-chai.use(chaiHttp);
 let assert = require('assert');
 
 describe('blbl()', function () {
 
-	it('exists', function () {
-		assert.equal(server('--ezda<>'),'&#151;ezda&lt;&gt;')
+	it('should return the html code of --', function () {
+		assert.equal(server.blbl('--'),'&#151;')
 	});
 	it('should return a space string', function () {
-		assert.equal(server(),' ')
+		assert.equal(server.blbl(),' ')
+	});
+	it('should return the html code of <',function(){
+		assert.equal(server.blbl('<'),'&lt;')
+	});
+	it('should return the html code of >',function(){
+		assert.equal(server.blbl('>'),'&gt;')
+	});
+	it('should return the html code of "',function(){
+		assert.equal(server.blbl('"'),'&quot;')
+	});
+	it('should return the html code of \'',function(){
+		assert.equal(server.blbl("'"),'&#039;')
+	});
+	it('should return the html code of &',function(){
+		assert.equal(server.blbl('&'),'&amp;')
 	});
 });
+
+server.bcrypt.hash('test',null,null,function(err,hash){
+	describe('Bcrypt hash',function(){
+		it('should return a different password',function(){
+			assert.notEqual(hash,'test')
+		});
+		it('should return true when compare the test string with the hash password',function(){
+			assert.equal(server.bcrypt.compareSync('test',hash),true)
+		})
+	})
+});
+
