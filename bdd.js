869df3ce2fba0531fffa98d3b1ad6817b2e665bd
Alencruser <38475565+Alencruser@users.noreply.github.com>
2018-10-04 13:42:37 +0200

    Delete bdd.js

--- bdd.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-let connection = mysql.createConnection({
-	host: 'den1.mysql1.gear.host',
-	user: 'stgo',
-	password: 'Ek7eg2-7?6Vc',
-	database: 'stgo'
-});
-module.exports= connection;
